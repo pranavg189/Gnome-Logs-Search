3034ddd5be5d0b01744c75187b9225e7284c6e02
Pranav Ganorkar <pranavg189@gmail.com>
2016-06-13 00:30:54 +0530

    Added Range Filtering option to Search Popover
    The range filtering option in search popover can used to set the timestamp range of journal.The default
    journal range is current boot. The query object was modified to include two new parameters for storing the
    start and end timestamps of the journal. The gl-journal-set-matches() was modified to remove setting the
    boot match if no boot match is specified.

--- src/gl-journal-model.h
+++ src/gl-journal-model.h
@@ -32,6 +32,8 @@ typedef struct GlQuery
 {
     GPtrArray *queryitems;   /* array of GlQueryItem objects passed through eventviewlist */
     gboolean is_search_field_exact;
+    guint64 start_timestamp;
+    guint64 end_timestamp;
 } GlQuery;
 
 #define GL_TYPE_JOURNAL_MODEL gl_journal_model_get_type()
@@ -48,6 +50,10 @@ void                    gl_query_add_match                              (GlQuery
                                                                          const gchar *field_name,
                                                                          const gchar *field_value,
                                                                          GlQuerySearchType search_type);
+
+void                    gl_query_set_journal_range                      (GlQuery *query,
+                                                                         guint64 start_timestamp,
+                                                                         guint64 end_timestamp);
 
 void                    gl_journal_model_set_matches                    (GlJournalModel      *model,
                                                                          const gchar * const *matches);
